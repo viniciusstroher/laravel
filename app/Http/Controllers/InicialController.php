e3ee402e7eae301668b453fd83d87c08674e4a4f
vinicius@rcx <vinicius@rcx>
2018-07-31 17:38:57 -0300

    init

---
+++ app/Http/Controllers/InicialController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InicialController extends Controller
+{
+    //
+    function index(){
+    	return ['teste'=>true];
+
+    }
+}
